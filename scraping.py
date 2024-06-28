48a28e6b38990b72a13db0c0248ac9d0a9d90dac
Daniel <vcdanielj@gmail.com>
2024-06-28 11:11:22 -0400

    Scraping con python

---
+++ scraping.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://es.wikipedia.org/wiki/Paraguay"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+content_list = soup.find_all("div", class_="vector-toc-text")
+
+content_text = []
+for content in content_list:
+    content_text.append(content.get_text())
+
+for i in content_text:
+    print(i)
